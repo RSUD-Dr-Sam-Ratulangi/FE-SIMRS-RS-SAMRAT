fbef43cf1db5857c38ac8a35bb9687955b8f0019
po3rdlp <leonpolandos53@gmail.com>
2023-10-06 19:29:51 +0800

    first commit

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const LoginPage = () => {
+    return (
+        <div>
+            <p>Hello This is Login Page</p>
+        </div>
+    )
+}
+
+export default LoginPage;
