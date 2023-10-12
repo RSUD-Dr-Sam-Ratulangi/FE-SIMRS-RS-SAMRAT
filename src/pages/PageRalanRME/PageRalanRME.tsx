242f03815f1f4676e6798e7b0a0f8c081125e740
po3rdlp <leonpolandos53@gmail.com>
2023-10-12 10:21:46 +0800

    update: -rawatjalan edit

--- src/pages/PageRalanRME/PageRalanRME.tsx
+++ src/pages/PageRalanRME/PageRalanRME.tsx
@@ -24,9 +24,69 @@ export default function PageRalanRME() {
   return (
     <div>
       <Breadcrumb />
-      <p> Hello {data.id}</p>
+      {/* <p> Hello {data.id}</p>
       <p>Tittle {data.title}</p>
-      <p>Price :{data.price}</p>
+      <p>Price :{data.price}</p> */}
+      <div className='w-full h-screen bg-white p-3'>
+        <p className='text text-lg font-bold pb-3'>Riwayat Perawatan</p>
+        <p className='font-light'>Data Riwayat Perawatan Pasien</p>
+        <div>
+          <div className='grid grid-cols-4 gap-10'>
+            <div>
+              <label className='label-text-alt font-light'>NO.RM</label>
+              <p className='text font-bold'>165647</p>
+            </div>
+            <div>
+              <label className='label-text-alt font-light'>UMUR</label>
+              <p className='text font-bold'>19 Th 10 Bl 18 Hr Tahun</p>
+            </div>
+            <div>
+              <label className='label-text-alt font-light'>GOLONGAN DARAH</label>
+              <p className='text font-bold'>-</p>
+            </div>
+            <div>
+              <label className='label-text-alt font-light'>AGAMA</label>
+              <p className='text font-bold'>Kristen</p>
+            </div>
+          </div>
+          <div className='grid grid-cols-4 gap-10'>
+            <div>
+              <label className='label-text-alt font-light'>Alamat</label>
+              <p className='text font-bold'>Ranomerut</p>
+            </div>
+            <div>
+              <label className='label-text-alt font-light'>Jenis Kelamin</label>
+              <p className='text font-bold'>Perempuan</p>
+            </div>
+            <div>
+              <label className='label-text-alt font-light'>Ibu Kandung</label>
+              <p className='text font-bold'>-</p>
+            </div>
+            <div>
+              <label className='label-text-alt font-light'>Pendidikan Terakhir</label>
+              <p className='text font-bold'>-</p>
+            </div>
+          </div>
+          <div className='grid grid-cols-4 gap-10'>
+            <div>
+              <label className='label-text-alt font-light'>NO.RM</label>
+              <p className='text font-bold'>165647</p>
+            </div>
+            <div>
+              <label className='label-text-alt font-light'>UMUR</label>
+              <p className='text font-bold'>19 Th 10 Bl 18 Hr Tahun</p>
+            </div>
+            <div>
+              <label className='label-text-alt font-light'>GOLONGAN DARAH</label>
+              <p className='text font-bold'>-</p>
+            </div>
+            <div>
+              <label className='label-text-alt font-light'>AGAMA</label>
+              <p className='text font-bold'>Kristen</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
