5d35b57f1db51f65c6499513c72677643640ccdd
rey112233 <rey.1111011110@gmail.com>
2023-10-17 10:57:46 +0800

    minor changes

--- src/components/Rme/soapPemeriksaanRalan.tsx
+++ src/components/Rme/soapPemeriksaanRalan.tsx
@@ -13,6 +13,75 @@ import {
 import TableDataNoSearch from '../Table/TableNoSearch'
 import { ROUTES } from '../../routes'
 
+const buttonsData = [
+  {
+    icon: <ArchiveBoxArrowDownIcon className='font-bold text-white h-[17px] w-[17px]' />,
+    text: 'Filter',
+    textColor: 'white',
+    bgColor: '[#55A46B]',
+  },
+  {
+    icon: <BellIcon className='font-bold text-disabled h-[17px] w-[17px]' />,
+    text: 'ICD 10 & 9',
+    textColor: 'disabled',
+    bgColor: 'white',
+  },
+  {
+    icon: <ClockIcon className='font-bold text-disabled h-[17px] w-[17px]' />,
+    text: 'Riwayat',
+    textColor: 'disabled',
+    bgColor: 'white',
+  },
+  {
+    icon: <CheckIcon className='font-bold text-disabled h-[17px] w-[17px]' />,
+    text: 'Selesai',
+    textColor: 'disabled',
+    bgColor: 'white',
+  },
+]
+
+const soapLabels = [
+  {
+    label: <label>Subjektif</label>,
+    input: (
+      <textarea className='w-full textarea textarea-bordered h-32 resize-none' placeholder='.-' />
+    ),
+  },
+  {
+    label: <label>Objektif</label>,
+    input: (
+      <textarea className='w-full textarea textarea-bordered h-32 resize-none' placeholder='.-' />
+    ),
+  },
+  {
+    label: <label>Asesmen</label>,
+    input: (
+      <textarea className='w-full textarea textarea-bordered h-32 resize-none' placeholder='.-' />
+    ),
+  },
+  {
+    label: <label>Plan</label>,
+    input: (
+      <textarea className='w-full textarea textarea-bordered h-32 resize-none' placeholder='.-' />
+    ),
+  },
+]
+
+const rawatPasienLabels = [
+  {
+    label: <label>ID Rawat</label>,
+    input: <input className='input input-bordered w-full' placeholder='1111111111111' />,
+  },
+  {
+    label: <label>Nomor Pasien</label>,
+    input: <input className='input input-bordered w-full' placeholder='1111111111111' />,
+  },
+  {
+    label: <label>Nomor RM</label>,
+    input: <input className='input input-bordered w-full' placeholder='1111111111111' />,
+  },
+]
+
 const SoapRalan = () => {
   const [data, setData] = useState()
 
@@ -152,9 +221,9 @@ const SoapRalan = () => {
           </p>
           <div className='flex gap-6 mt-4 mb-6'>
             <div className='w-[70%]'>
-              <div className='flex gap-6 mb-4'>
-                <div className='flex gap-4 w-[50%] '>
-                  <div className='w-[60%] flex flex-col'>
+              <div className='grid grid-cols-2 gap-6 p-4'>
+                <div className='col-span-1 flex gap-4'>
+                  <div className='w-[60%] flex flex-col '>
                     <label>Tanggal</label>
                     <input type='date' className='input input-bordered w-full' />
                   </div>
@@ -166,58 +235,22 @@ const SoapRalan = () => {
                     </div>
                   </div>
                 </div>
-                <div className='w-[50%]'>
-                  <div className='flex flex-col'>
-                    <label>ID Rawat</label>
-                    <input className='input input-bordered w-full' placeholder='1111111111111' />
+                {rawatPasienLabels.map((label, index) => (
+                  <div key={index} className='flex flex-col'>
+                    {label.label}
+                    {label.input}
                   </div>
-                </div>
+                ))}
               </div>
-              <div className='flex gap-6'>
-                <div className='w-[50%]'>
-                  <div className='flex flex-col'>
-                    <label>Nama Pasien</label>
-                    <input className='input input-bordered w-full' placeholder='Esthera Jackson' />
-                  </div>
-                </div>
-                <div className='w-[50%]'>
-                  <div className='flex flex-col'>
-                    <label>Nomor RIM</label>
-                    <input
-                      className='input input-bordered w-full'
-                      placeholder='00000/00000/00000'
-                    />
-                  </div>
-                </div>
-              </div>
+
               <p className='font-sans text-xl font-bold text-[#2D3748] mt-6'>Soap</p>
-              <div className='flex gap-6 mb-4'>
-                <div className='w-[50%]'>
-                  <div className='flex flex-col'>
-                    <label>Subjektif</label>
-                    <input className='input input-bordered w-full h-32' placeholder='.-' />
+              <div className='grid grid-cols-2 gap-6 p-4'>
+                {soapLabels.map((label, index) => (
+                  <div key={index} className='flex flex-col'>
+                    {label.label}
+                    {label.input}
                   </div>
-                </div>
-                <div className='w-[50%]'>
-                  <div className='flex flex-col'>
-                    <label>Objektif</label>
-                    <input className='input input-bordered w-full h-32' placeholder='.-' />
-                  </div>
-                </div>
-              </div>
-              <div className='flex gap-6 '>
-                <div className='w-[50%]'>
-                  <div className='flex flex-col'>
-                    <label>Asesmen</label>
-                    <input className='input input-bordered w-full h-32' placeholder='.-' />
-                  </div>
-                </div>
-                <div className='w-[50%]'>
-                  <div className='flex flex-col'>
-                    <label>Plan</label>
-                    <input className='input input-bordered w-full h-32' placeholder='.-' />
-                  </div>
-                </div>
+                ))}
               </div>
             </div>
             <div className=' w-[30%]'>
@@ -230,34 +263,21 @@ const SoapRalan = () => {
                   Mohon pastikan data yang anda masukan sudah benar sebelum melanjutkan. Kesalahan
                   dalam pengisian data dapat berdampak pada perawatan pasiens
                 </p>
-                <button tabIndex={0} className='flex btn w-full text-white bg-[#55A46B]'>
-                  <ArchiveBoxArrowDownIcon className='font-bold text-white h-[17px] w-[17px]' />
-                  Filter
-                </button>
-                <button
-                  tabIndex={0}
-                  className='flex btn w-full text-disabled bg-white border-2 border-disabled'
-                >
-                  <BellIcon className='font-bold text-disabled h-[17px] w-[17px]' />
-                  ICD 10 & 9
-                </button>
-                <button
-                  tabIndex={0}
-                  className='flex btn w-full text-disabled bg-white border-2 border-disabled'
-                >
-                  <ClockIcon className='font-bold text-disabled h-[17px] w-[17px]' />
-                  Riwayat
-                </button>
-                <button
-                  tabIndex={0}
-                  className='flex btn w-full text-disabled bg-white border-2 border-disabled'
-                >
-                  <CheckIcon className='font-bold text-disabled h-[17px] w-[17px]' />
-                  Selesai
-                </button>
+                {buttonsData.map((button, index) => (
+                  <button
+                    key={index}
+                    tabIndex={0}
+                    className={`flex btn w-full text-${button.textColor} bg-${button.bgColor} border-2 border-${button.textColor}`}
+                  >
+                    {button.icon}
+                    {button.text}
+                  </button>
+                ))}
               </div>
             </div>
           </div>
+        </div>
+        <div className='bg-white h-auto w-full p-4 mt-6 rounded-xl'>
           <p className='font-sans text-base font-bold text-[#2D3748]'>Rincian riwayat</p>
           <TableDataNoSearch data={data} columns={columns} />
         </div>
