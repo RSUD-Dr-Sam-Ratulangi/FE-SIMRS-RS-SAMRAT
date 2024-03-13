f3f0da6a46ff86692f254ecf38acff9cdf67b987
po3rdlp <leonpolandos53@gmail.com>
2024-03-13 10:55:27 +0800

    fix

--- src/services/api/config.api.ts
+++ src/services/api/config.api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://rsudsamrat.site:8991/',
+  baseURL: 'http://rsudsamrat.site:8901/',
   timeout: 120000,
   headers: {
     'Content-Type': 'application/json',
